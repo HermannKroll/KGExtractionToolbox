2fa4b7c2bbcfd9362d35359d1f35841f5b257993
HermannKroll <hermann.kroll@gmail.com>
2022-03-04 12:10:25 +0100

    retrieve tagged documents from db with all info

---
+++ src/kgextractiontoolbox/backend/retrieve.py
@@ -0,0 +1,65 @@
+from collections import defaultdict
+from typing import List, Set
+
+from sqlalchemy import and_
+
+import kgextractiontoolbox.document.document
+from kgextractiontoolbox.backend.models import Document, DocumentClassification, Tag, DocumentSection
+from kgextractiontoolbox.document.document import TaggedDocument, TaggedEntity
+
+
+def retrieve_tagged_documents_from_database(session, document_ids: Set[int], document_collection: str) \
+        -> List[TaggedDocument]:
+    """
+    Retrieves a set of TaggedDocuments from the database
+    :param session: the current session
+    :param document_ids: a set of document ids
+    :param document_collection: the corresponding document collection
+    :return: a list of TaggedDocuments
+    """
+    doc_results = {}
+
+    # first query document titles and abstract
+    doc_query = session.query(Document).filter(and_(Document.id.in_(document_ids),
+                                                    Document.collection == document_collection))
+
+    for res in doc_query:
+        doc_results[res.id] = TaggedDocument(id=res.id, title=res.title, abstract=res.abstract)
+
+    # Next query the publication information
+    classification_query = session.query(DocumentClassification).filter(
+        and_(DocumentClassification.document_id.in_(document_ids),
+             DocumentClassification.document_collection == document_collection))
+    doc2classification = defaultdict(set)
+    for res in classification_query:
+        doc2classification[res.document_id].add((res.classification, res.explanation))
+
+    # Query for Document sections
+    sec_query = session.query(DocumentSection).filter(and_(DocumentSection.document_id.in_(document_ids),
+                                                           DocumentSection.document_collection == document_collection))
+    for res_sec in sec_query:
+        doc_results[res_sec.document_id].sections.append(kgextractiontoolbox.document.document.DocumentSection(
+            position=res_sec.position,
+            title=res_sec.title,
+            text=res_sec.text
+        ))
+
+    # Next query for all tagged entities in that document
+    tag_query = session.query(Tag).filter(and_(Tag.document_id.in_(document_ids),
+                                               Tag.document_collection == document_collection))
+    tag_result = defaultdict(list)
+    for res in tag_query:
+        tag_result[res.document_id].append(TaggedEntity(document=res.document_id,
+                                                        start=res.start,
+                                                        end=res.end,
+                                                        ent_id=res.ent_id,
+                                                        ent_type=res.ent_type,
+                                                        text=res.ent_str))
+    for doc_id, tags in tag_result.items():
+        doc_results[doc_id].tags = tags
+        doc_results[doc_id].sort_tags()
+
+    for doc_id, classification in doc2classification.items():
+        doc_results[doc_id].classification = {d_class: d_expl for d_class, d_expl in classification}
+
+    return list(doc_results.values())
