18849a170326dd5876e61425599cc41ec8d4a02a
HermannKroll <hermann.kroll@gmail.com>
2022-10-25 21:09:06 +0200

    multilingual case study: scripts to analyze docs + extractiions

--- case_studies/multilingual/compare_translation.py
+++ case_studies/multilingual/compare_translation.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from argparse import ArgumentParser
 
 from spacy.lang.en import English
@@ -10,6 +11,25 @@ from kgextractiontoolbox.extraction.extraction_utils import filter_document_sent
 
 EN_ABSTRACT = "1"
 DEEPL_ABSTRACT = "2"
+
+COMPLEX_REGEX_PUNCTUATION = re.compile(r'[,.;|&:?!]+', re.IGNORECASE)
+COMPLEX_REGEX_WORDS = re.compile(r'[^\w](and|or|that|which|who|what|because|de|thus|hence)+[^\w]', re.IGNORECASE)
+
+
+def check_sent_is_complex(phrase: str):
+    if not phrase:
+        return False
+    # remove tailing punctuation
+    while COMPLEX_REGEX_PUNCTUATION.match(phrase[-1]):
+        phrase = phrase[:-1]
+        if not phrase:
+            return False
+    phrase = f' {phrase} '
+
+    if COMPLEX_REGEX_PUNCTUATION.search(phrase) or COMPLEX_REGEX_WORDS.search(phrase):
+        return True
+    else:
+        return False
 
 
 def count_document_sentences(file: str):
@@ -32,7 +52,7 @@ def count_document_sentences(file: str):
 
     for label, tagged_docs in [("English", english_docs), ("Translated", translated_docs)]:
         doc_ids = set([d.id for d in tagged_docs])
-        logging.info('=='*60)
+        logging.info('==' * 60)
         logging.info(f'Document type: {label}')
         sentence_count = sum([len(doc.sentence_by_id) for doc in tagged_docs])
         logging.info(f'Found {len(doc_ids)} documents')
@@ -44,15 +64,17 @@ def count_document_sentences(file: str):
         doc2tags = {d: s for d, s in doc2tags.items() if d in doc_ids}
 
         tag_count = sum([len(tags) for doc_id, tags in doc2tags.items()])
-        sentence_count = sum([len(sents) for doc_id, sents in doc2sentences.items()])
+        sentence_w2e_count = sum([len(sents) for doc_id, sents in doc2sentences.items()])
         # NER tags are upper case (for Stanza) -> EL not
         ner_count = len([t for _, tags in doc2tags.items() for t in tags if t.ent_type == t.ent_type.upper()])
         el_count = len([t for _, tags in doc2tags.items() for t in tags if t.ent_type != t.ent_type.upper()])
         logging.info(f'Found {tag_count} tags in sum ({ner_count} NER + {el_count} EL)')
-        logging.info(f'Found {sentence_count} sentences with at least two entities')
+        logging.info(f'Found {sentence_w2e_count} sentences with at least two entities')
+        complex_sent_count = len(
+            [s for d in tagged_docs for s in d.sentence_by_id.values() if check_sent_is_complex(s.text.strip())])
+        logging.info(f'Found {complex_sent_count} complex sentences ({complex_sent_count / sentence_count}%)')
 
-
-        logging.info('=='*60)
+    logging.info('==' * 60)
 
 
 def main():
