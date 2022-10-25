61b5a04deaac3785525bf193ada832fdaed0596d
HermannKroll <hermann.kroll@gmail.com>
2022-10-25 20:59:36 +0200

    support counting of complex sentences

--- src/kgextractiontoolbox/document/count_sentences.py
+++ src/kgextractiontoolbox/document/count_sentences.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from argparse import ArgumentParser
 
 from spacy.lang.en import English
@@ -7,6 +8,25 @@ from kgextractiontoolbox.document.count import count_documents
 from kgextractiontoolbox.document.document import TaggedDocument
 from kgextractiontoolbox.document.extract import read_pubtator_documents
 from kgextractiontoolbox.extraction.extraction_utils import filter_document_sentences_without_tags
+
+COMPLEX_REGEX_PUNCTUATION = re.compile(r'[,.;|&:?!]+', re.IGNORECASE)
+COMPLEX_REGEX_WORDS = re.compile(r'[^\w](and|or|that|which|who|what|because|de|thus|hence)+[^\w]', re.IGNORECASE)
+
+
+def check_sent_is_complex(phrase: str):
+    if not phrase:
+        return False
+    # remove tailing punctuation
+    while COMPLEX_REGEX_PUNCTUATION.match(phrase[-1]):
+        phrase = phrase[:-1]
+        if not phrase:
+            return False
+    phrase = f' {phrase} '
+
+    if COMPLEX_REGEX_PUNCTUATION.search(phrase) or COMPLEX_REGEX_WORDS.search(phrase):
+        return True
+    else:
+        return False
 
 
 def count_document_sentences(file: str):
@@ -28,9 +48,12 @@ def count_document_sentences(file: str):
 
     doc2sentences, doc2tags = filter_document_sentences_without_tags(doc_count, file, spacy_nlp)
     tag_count = sum([len(tags) for doc_id, tags in doc2tags.items()])
-    sentence_count = sum([len(sents) for doc_id, sents in doc2sentences.items()])
+    sentence_w2e_count = sum([len(sents) for doc_id, sents in doc2sentences.items()])
+    complex_sent_count = len(
+        [s for d in tagged_docs for s in d.sentence_by_id.values() if check_sent_is_complex(s.text.strip())])
     logging.info(f'Found {tag_count} tags')
-    logging.info(f'Found {sentence_count} sentences with at least two entities')
+    logging.info(f'Found {sentence_w2e_count} sentences with at least two entities')
+    logging.info(f'Found {complex_sent_count} complex sentences ({complex_sent_count / sentence_count}%)')
 
 
 def main():
