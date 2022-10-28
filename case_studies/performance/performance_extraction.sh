feaf66b40925a23881759f3087d0f5ab424645bd
HermannKroll <hermann.kroll@gmail.com>
2022-10-28 12:27:48 +0200

    add Wikipedia PathIE and change order

--- case_studies/performance/performance_extraction.sh
+++ case_studies/performance/performance_extraction.sh
@@ -8,11 +8,58 @@ WIKIPEDIA_DOC="../data/wikipedia/wikipedia_scientists.json"
 WIKIPEDIA_DOC_ENTITIES="../data/wikipedia/wikipedia_scientists_entities.json"
 WIKIPEDIA_OPENIE6_EXTRACATIONS_TEST="../data/wikipedia/openie6_benchmark.tsv"
 
+WIKIPEDIA_RELATION_VOCAB_SMALL="../data/wikipedia/relation_vocab_small.json"
 
 PUBMED_SAMPLE="../data/pubmed/pubmed_10k_with_entities.json"
 PUBMED_OPENIE6_TEMP="../data/pubmed/openie6_performance.tsv"
-PATHIE_OUTPUT_TEMP="../data/pubmed/pathie_performance.tsv"
+WIKIPEDIA_PATHIE_OUTPUT_TEMP="../data/wikipedia/pathie_performance.tsv"
+PUBMED_PATHIE_OUTPUT_TEMP="../data/pubmed/pathie_performance.tsv"
 PUBMED_RELATION_VOCAB="../data/pubmed/pharm_relation_vocab.json"
+
+
+
+START=$(date +%s.%N)
+python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/pathie/main.py $WIKIPEDIA_DOC_ENTITIES $WIKIPEDIA_PATHIE_OUTPUT_TEMP --relation_vocab $WIKIPEDIA_RELATION_VOCAB_SMALL --workers 32 2>> /dev/null 1>>/dev/null
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo "1. PathIE Wikipedia: ${DIFF}s"
+
+
+START=$(date +%s.%N)
+python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/pathie/main.py $WIKIPEDIA_DOC_ENTITIES $WIKIPEDIA_PATHIE_OUTPUT_TEMP --relation_vocab $PUBMED_RELATION_VOCAB --workers 32 2>> /dev/null 1>>/dev/null
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo "2. PathIE Wikipedia: ${DIFF}s"
+
+
+START=$(date +%s.%N)
+python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/pathie/main.py $WIKIPEDIA_DOC_ENTITIES $WIKIPEDIA_PATHIE_OUTPUT_TEMP --relation_vocab $PUBMED_RELATION_VOCAB --workers 32 2>> /dev/null 1>>/dev/null
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo "3. PathIE Wikipedia: ${DIFF}s"
+
+
+START=$(date +%s.%N)
+python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/pathie/main.py $PUBMED_SAMPLE $PUBMED_PATHIE_OUTPUT_TEMP --relation_vocab $PUBMED_RELATION_VOCAB --workers 32 2>> /dev/null 1>>/dev/null
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo "1. PathIE PubMed: ${DIFF}s"
+
+
+START=$(date +%s.%N)
+python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/pathie/main.py $PUBMED_SAMPLE $PUBMED_PATHIE_OUTPUT_TEMP --relation_vocab $PUBMED_RELATION_VOCAB --workers 32 2>> /dev/null 1>>/dev/null
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo "2. PathIE PubMed: ${DIFF}s"
+
+
+START=$(date +%s.%N)
+python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/pathie/main.py $PUBMED_SAMPLE $PUBMED_PATHIE_OUTPUT_TEMP --relation_vocab $PUBMED_RELATION_VOCAB --workers 32 2>> /dev/null 1>>/dev/null
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo "3. PathIE PubMed: ${DIFF}s"
+
+
 
 START=$(date +%s.%N)
 python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/openie6/main.py $POLLUX_DOC $POLLUX_OPENIE6_EXTRACATIONS_TEST  --no_entity_filter 2>> /dev/null 1>>/dev/null
@@ -105,25 +152,6 @@ echo "3. OpenIE6 EF Wikipedia: ${DIFF}s"
 
 
 
-START=$(date +%s.%N)
-python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/pathie/main.py $PUBMED_SAMPLE $PATHIE_OUTPUT_TEMP --relation_vocab $PUBMED_RELATION_VOCAB --workers 32 2>> /dev/null 1>>/dev/null
-END=$(date +%s.%N)
-DIFF=$(echo "$END - $START" | bc)
-echo "1. PathIE PubMed: ${DIFF}s"
-
-
-START=$(date +%s.%N)
-python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/pathie/main.py $PUBMED_SAMPLE $PATHIE_OUTPUT_TEMP --relation_vocab $PUBMED_RELATION_VOCAB --workers 32 2>> /dev/null 1>>/dev/null
-END=$(date +%s.%N)
-DIFF=$(echo "$END - $START" | bc)
-echo "2. PathIE PubMed: ${DIFF}s"
-
-
-START=$(date +%s.%N)
-python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/pathie/main.py $PUBMED_SAMPLE $PATHIE_OUTPUT_TEMP --relation_vocab $PUBMED_RELATION_VOCAB --workers 32 2>> /dev/null 1>>/dev/null
-END=$(date +%s.%N)
-DIFF=$(echo "$END - $START" | bc)
-echo "3. PathIE PubMed: ${DIFF}s"
 
 
 START=$(date +%s.%N)
