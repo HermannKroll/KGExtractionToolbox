82b6511b4207fa8543888ef7cd716d8d91fbc10f
HermannKroll <hermann.kroll@gmail.com>
2022-10-28 12:24:27 +0200

    short entities aren't deleted for pollux

--- case_studies/canonicalization_advanced/pollux_scripts.sh
+++ case_studies/canonicalization_advanced/pollux_scripts.sh
@@ -9,11 +9,8 @@ POLLUX_OPENIE6_EXTRACATIONS="../data/pollux/openie6.tsv"
 # Load document content
 python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/document/load_document.py $POLLUX_DOC_ENTITIES -c pollux_canon
 
-# Next Delete all short entities
-python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/entitylinking/delete_short_tags.py 5 -c pollux_canon
-
 # run OpenIE6
-# python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/openie6/main.py $POLLUX_DOC $POLLUX_OPENIE6_EXTRACATIONS  --no_entity_filter
+python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/openie6/main.py $POLLUX_DOC $POLLUX_OPENIE6_EXTRACATIONS  --no_entity_filter
 
 # Load OpenIE6
 python3 ~/KGExtractionToolbox/src/kgextractiontoolbox/extraction/loading/load_openie_extractions.py $POLLUX_OPENIE6_EXTRACATIONS -c pollux_canon -et OPENIE6_NF --entity_filter no_entity_filter
