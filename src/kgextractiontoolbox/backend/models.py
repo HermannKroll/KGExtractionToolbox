38e3a16bf637b33bc5d35dc57ba4c4833f20648f
HermannKroll <hermann.kroll@gmail.com>
2023-01-12 09:04:22 +0100

    activate unique constraint again (cleaner DB)

--- src/kgextractiontoolbox/backend/models.py
+++ src/kgextractiontoolbox/backend/models.py
@@ -192,8 +192,8 @@ class Tag(Base, DatabaseTable):
         ForeignKeyConstraint(('document_id', 'document_collection'), ('document.id', 'document.collection'),
                              sqlite_on_conflict='IGNORE'),
         # Todo: In real-word scenarios this index become very large
- #       UniqueConstraint('document_id', 'document_collection', 'start', 'end', 'ent_type', 'ent_id',
- #                        sqlite_on_conflict='IGNORE'),
+        UniqueConstraint('document_id', 'document_collection', 'start', 'end', 'ent_type', 'ent_id',
+                         sqlite_on_conflict='IGNORE'),
         PrimaryKeyConstraint('id', sqlite_on_conflict='IGNORE')
     )
 
