a79e353710506c76c9734c09c9ca11564a924444
Bill Matthias Thang <m.thang@tu-bs.de>
2023-09-12 10:56:46 +0200

    #181 added tests for ignoring already tagged documents and usage of force parameter

--- src/kgtests/src/entitylinking/tagging/test_vocab_entity_linking.py
+++ src/kgtests/src/entitylinking/tagging/test_vocab_entity_linking.py
@@ -12,7 +12,6 @@ class TestVocabEntityLinking(unittest.TestCase):
         cls.collection = "test_collection"
         cls.vocabulary_file = util.resource_rel_path('vocabs/test_vocab.tsv')
         cls.input_file = util.resource_rel_path('infiles/test_vocab_dictpreprocess/abbreviation_test_allowed.txt')
-        cls.expansion_file = util.resource_rel_path('infiles/test_vocab_dictpreprocess/expansion_test.txt')
 
     def test_tag_collection_without_input_file(self):
         util.clear_database()
@@ -27,18 +26,32 @@ class TestVocabEntityLinking(unittest.TestCase):
         util.clear_database()
 
     def test_ignore_already_tagged_documents(self):
+        with self.assertRaises(SystemExit):
+            util.clear_database()
+            args = [
+                *f"-i {self.input_file} -c {self.collection} -v {self.vocabulary_file} --loglevel DEBUG --workdir {self.workdir} -w 1 -y".split()
+            ]
+            vdp.main(args)
+            #util.clear_doc_tagged_by_table()
+            print(set(util.get_docs_tagged_by_from_database()))
+            args = [
+                *f"-i {self.input_file} -c {self.collection} -v {self.vocabulary_file} --loglevel DEBUG --workdir {self.workdir} -w 1 -y".split()
+            ]
+            vdp.main(args)
+            util.clear_database()
+
+    def test_force_parameter_for_tagging(self):
         util.clear_database()
         args = [
             *f"-i {self.input_file} -c {self.collection} -v {self.vocabulary_file} --loglevel DEBUG --workdir {self.workdir} -w 1 -y".split()
         ]
         vdp.main(args)
-        docs_tagged_by1 = set(util.get_docs_tagged_by_from_database())
-        util.clear_doc_tagged_by_table()
-
+        tags1 = set(util.get_docs_tagged_by_from_database())
+        # Attempt to tag the same input file with force parameter
         args = [
-            *f"-i {self.expansion_file} -c {self.collection} -v {self.vocabulary_file} --loglevel DEBUG --workdir {self.workdir} -w 1 -y".split()
+            *f"-i {self.input_file} -c {self.collection} -v {self.vocabulary_file} --loglevel DEBUG --workdir {self.workdir} -w 1 -y -f".split()
         ]
         vdp.main(args)
-        docs_tagged_by2 = set(util.get_docs_tagged_by_from_database())
-        self.assertEqual(0, len(docs_tagged_by1.intersection(docs_tagged_by2)))
+        tags2 = set(util.get_docs_tagged_by_from_database())
+        self.assertEqual(tags1, tags2)
         util.clear_database()
