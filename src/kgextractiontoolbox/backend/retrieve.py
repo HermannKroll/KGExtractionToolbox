b3335673f031be8b5b3fda42ff4fb27468776aca
HermannKroll <hermann.kroll@gmail.com>
2024-03-02 14:04:32 +0100

    support a new range-based query mode for faster iterating over documents + refactor narrative documents

--- src/kgextractiontoolbox/backend/retrieve.py
+++ src/kgextractiontoolbox/backend/retrieve.py
@@ -1,14 +1,33 @@
+import logging
 from collections import defaultdict
 from typing import List, Set
 
-from sqlalchemy import and_
-
 import kgextractiontoolbox.document.document
 from kgextractiontoolbox.backend.models import Document, DocumentClassification, Tag, DocumentSection, \
-    BULK_QUERY_CURSOR_COUNT_DEFAULT, DocumentMetadata, Predication, Sentence
-from kgextractiontoolbox.document.document import TaggedDocument, TaggedEntity, DocumentSection as ds
+    DocumentMetadata, Predication, Sentence
+from kgextractiontoolbox.document.document import TaggedDocument, TaggedEntity
 from kgextractiontoolbox.document.narrative_document import NarrativeDocument, NarrativeDocumentMetadata, \
     StatementExtraction, DocumentSentence
+
+
+def should_use_range_mode(document_ids: List[int]) -> (bool, int, int):
+    """
+    if the diff between highest and lowest id corresponds to the number of document ids
+    a range query should be way faster than a large IN expression
+    note that we do not ensure that all ids are given (so 20% of missing documents in between are ok)
+    :param document_ids: a sorted list of document ids
+    :return:
+    """
+    highest_id = document_ids[-1]
+    lowest_id = document_ids[0]
+    if lowest_id > highest_id:
+        raise ValueError(f'Document id list must be supported')
+
+    if 1000 < len(document_ids) and len(document_ids) > (highest_id - lowest_id) * 0.8:
+        logging.info(f"Using fast range mode to filter for document ids ({lowest_id} - {highest_id})")
+        return True, lowest_id, highest_id
+    else:
+        return False, lowest_id, highest_id
 
 
 def iterate_over_all_documents_in_collection(session, collection: str, document_ids=None, consider_tag=False,
@@ -31,20 +50,28 @@ def iterate_over_all_documents_in_collection(session, collection: str, document_
 
     if document_ids:
         document_ids = sorted(list(document_ids))
-        doc_query = doc_query.filter(Document.id.in_(document_ids))
+        enable_range_mode, lowest_id, highest_id = should_use_range_mode(document_ids)
+
+        if enable_range_mode:
+            # ensure that we have ids here
+            document_ids_set = set({int(did) for did in document_ids})
+            doc_query = doc_query.filter(Document.id.between(lowest_id, highest_id))
+        else:
+            doc_query = doc_query.filter(Document.id.in_(document_ids))
 
     doc_query = doc_query.order_by(Document.id)
-    doc_query = doc_query.yield_per(BULK_QUERY_CURSOR_COUNT_DEFAULT)
 
     if consider_tag:
         tag_query = session.query(Tag)
         tag_query = tag_query.filter(Tag.document_collection == collection)
 
         if document_ids:
-            tag_query = tag_query.filter(Tag.document_id.in_(document_ids))
+            if enable_range_mode:
+                tag_query = tag_query.filter(Tag.document_id.between(lowest_id, highest_id))
+            else:
+                tag_query = tag_query.filter(Tag.document_id.in_(document_ids))
 
         tag_query = tag_query.order_by(Tag.document_id)
-        tag_query = tag_query.yield_per(BULK_QUERY_CURSOR_COUNT_DEFAULT)
         tag_query = iter(tag_query)
         current_tag = next(tag_query, None)
 
@@ -53,10 +80,12 @@ def iterate_over_all_documents_in_collection(session, collection: str, document_
         class_query = class_query.filter(DocumentClassification.document_collection == collection)
 
         if document_ids:
-            class_query = class_query.filter(DocumentClassification.document_id.in_(document_ids))
+            if enable_range_mode:
+                class_query = class_query.filter(DocumentClassification.document_id.between(lowest_id, highest_id))
+            else:
+                class_query = class_query.filter(DocumentClassification.document_id.in_(document_ids))
 
         class_query = class_query.order_by(DocumentClassification.document_id)
-        class_query = class_query.yield_per(BULK_QUERY_CURSOR_COUNT_DEFAULT)
 
         class_query = iter(class_query)
         current_class = next(class_query, None)
@@ -66,10 +95,12 @@ def iterate_over_all_documents_in_collection(session, collection: str, document_
         sec_query = sec_query.filter(DocumentSection.document_collection == collection)
 
         if document_ids:
-            sec_query = sec_query.filter(DocumentSection.document_id.in_(document_ids))
+            if enable_range_mode:
+                sec_query = sec_query.filter(DocumentSection.document_id.between(lowest_id, highest_id))
+            else:
+                sec_query = sec_query.filter(DocumentSection.document_id.in_(document_ids))
 
         sec_query = sec_query.order_by(DocumentSection.document_id)
-        sec_query = sec_query.yield_per(BULK_QUERY_CURSOR_COUNT_DEFAULT)
         sec_query = iter(sec_query)
         current_sec = next(sec_query, None)
 
@@ -101,7 +132,13 @@ def iterate_over_all_documents_in_collection(session, collection: str, document_
                 current_sec = next(sec_query, None)
 
         t_doc.remove_duplicates_and_sort_tags()
-        yield t_doc
+
+        # if we queried with a range mode, we need to ensure that the document ids are really needed
+        if document_ids and enable_range_mode:
+            if t_doc.id in document_ids_set:
+                yield t_doc
+        else:
+            yield t_doc
 
 
 def retrieve_tagged_documents_from_database(session, document_ids: Set[int], document_collection: str) \
@@ -116,25 +153,53 @@ def retrieve_tagged_documents_from_database(session, document_ids: Set[int], doc
     doc_results = {}
 
     document_ids = sorted(list(document_ids))
+    enable_range_mode, lowest_id, highest_id = should_use_range_mode(document_ids)
+
     # first query document titles and abstract
-    doc_query = session.query(Document).filter(and_(Document.id.in_(document_ids),
-                                                    Document.collection == document_collection))
+    doc_query = session.query(Document)
+    doc_query = doc_query.filter(Document.collection == document_collection)
+    if enable_range_mode:
+        # ensure that we have ids here
+        document_ids_set = set({int(did) for did in document_ids})
+        doc_query = doc_query.filter(Document.id.between(lowest_id, highest_id))
+    else:
+        doc_query = doc_query.filter(Document.id.in_(document_ids))
 
     for res in doc_query:
+        if enable_range_mode and res.id not in document_ids_set:
+            continue
         doc_results[res.id] = TaggedDocument(id=res.id, title=res.title, abstract=res.abstract)
 
-    # Next query the publication information
-    classification_query = session.query(DocumentClassification).filter(
-        and_(DocumentClassification.document_id.in_(document_ids),
-             DocumentClassification.document_collection == document_collection))
+    # Next query the classification information
+    classification_query = session.query(DocumentClassification)
+    classification_query = classification_query.filter(
+        DocumentClassification.document_collection == document_collection)
+
+    if enable_range_mode:
+        classification_query = classification_query.filter(
+            DocumentClassification.document_id.between(lowest_id, highest_id))
+    else:
+        classification_query = classification_query.filter(DocumentClassification.document_id.in_(document_ids))
+
     doc2classification = defaultdict(set)
     for res in classification_query:
+        if enable_range_mode and res.document_id not in document_ids_set:
+            continue
         doc2classification[res.document_id].add((res.classification, res.explanation))
 
     # Query for Document sections
-    sec_query = session.query(DocumentSection).filter(and_(DocumentSection.document_id.in_(document_ids),
-                                                           DocumentSection.document_collection == document_collection))
+    sec_query = session.query(DocumentSection)
+    sec_query = sec_query.filter(DocumentSection.document_collection == document_collection)
+
+    if enable_range_mode:
+        sec_query = sec_query.filter(DocumentSection.document_id.between(lowest_id, highest_id))
+    else:
+        sec_query = sec_query.filter(DocumentSection.document_id.in_(document_ids))
+
     for res_sec in sec_query:
+        if enable_range_mode and res_sec.document_id not in document_ids_set:
+            continue
+
         doc_results[res_sec.document_id].sections.append(kgextractiontoolbox.document.document.DocumentSection(
             position=res_sec.position,
             title=res_sec.title,
@@ -142,10 +207,18 @@ def retrieve_tagged_documents_from_database(session, document_ids: Set[int], doc
         ))
 
     # Next query for all tagged entities in that document
-    tag_query = session.query(Tag).filter(and_(Tag.document_id.in_(document_ids),
-                                               Tag.document_collection == document_collection))
+    tag_query = session.query(Tag)
+    tag_query = tag_query.filter(Tag.document_collection == document_collection)
+    if enable_range_mode:
+        tag_query = tag_query.filter(Tag.document_id.between(lowest_id, highest_id))
+    else:
+        tag_query = tag_query.filter(Tag.document_id.in_(document_ids))
+
     tag_result = defaultdict(list)
     for res in tag_query:
+        if enable_range_mode and res.document_id not in document_ids_set:
+            continue
+
         tag_result[res.document_id].append(TaggedEntity(document=res.document_id,
                                                         start=res.start,
                                                         end=res.end,
@@ -171,31 +244,30 @@ def retrieve_narrative_documents_from_database(session, document_ids: Set[int], 
     :param document_collection: the corresponding document collection
     :return: a list of NarrativeDocuments
     """
-    doc_results = {}
+    tagged_docs = retrieve_tagged_documents_from_database(session, document_ids, document_collection)
 
-    # logging.info(f'Querying {len(document_ids)} from collection: {document_collection}...')
-    # first query document titles and abstract
-    doc_query = session.query(Document).filter(and_(Document.id.in_(document_ids),
-                                                    Document.collection == document_collection))
+    doc_results = {d.id: NarrativeDocument(document_id=d.id,
+                                           title=d.title,
+                                           abstract=d.abstract,
+                                           tags=d.tags,
+                                           classification=d.classification,
+                                           sections=d.sections) for d in tagged_docs}
 
-    for res in doc_query:
-        doc_results[res.id] = NarrativeDocument(document_id=res.id, title=res.title, abstract=res.abstract)
+    document_ids = sorted(list(document_ids))
+    enable_range_mode, lowest_id, highest_id = should_use_range_mode(document_ids)
 
-    #   logging.info('Querying for document classification...')
-    # Next query the publication information
-    classification_query = session.query(DocumentClassification).filter(
-        and_(DocumentClassification.document_id.in_(document_ids),
-             DocumentClassification.document_collection == document_collection))
-    doc2classification = defaultdict(set)
-    for res in classification_query:
-        doc2classification[res.document_id].add((res.classification, res.explanation))
+    # Next query the metadata information
+    metadata_query = session.query(DocumentMetadata)
+    metadata_query = metadata_query.filter(DocumentMetadata.document_collection == document_collection)
+    if enable_range_mode:
+        metadata_query = metadata_query.filter(DocumentMetadata.document_id.between(lowest_id, highest_id))
+    else:
+        metadata_query = metadata_query.filter(DocumentMetadata.document_id.in_(document_ids))
 
-    #    logging.info('Querying for metadata...')
-    # Next query the publication information
-    metadata_query = session.query(DocumentMetadata).filter(and_(DocumentMetadata.document_id.in_(document_ids),
-                                                                 DocumentMetadata.document_collection == document_collection))
     doc2metadata = {}
     for res in metadata_query:
+        if enable_range_mode and res.document_id not in doc_results:
+            continue
         metadata = NarrativeDocumentMetadata(publication_year=res.publication_year,
                                              publication_month=res.publication_month,
                                              authors=res.authors,
@@ -203,35 +275,11 @@ def retrieve_narrative_documents_from_database(session, document_ids: Set[int], 
                                              publication_doi=res.publication_doi)
         doc2metadata[res.document_id] = metadata
 
-    # Query for Document sections
-    sec_query = session.query(DocumentSection).filter(and_(DocumentSection.document_id.in_(document_ids),
-                                                           DocumentSection.document_collection == document_collection))
-    for res_sec in sec_query:
-        doc_results[res_sec.document_id].sections.append(ds(
-            position=res_sec.position,
-            title=res_sec.title,
-            text=res_sec.text
-        ))
-    #  logging.info('Querying for tags...')
-    # Next query for all tagged entities in that document
-    tag_query = session.query(Tag).filter(and_(Tag.document_id.in_(document_ids),
-                                               Tag.document_collection == document_collection))
-    tag_result = defaultdict(list)
-    for res in tag_query:
-        tag_result[res.document_id].append(TaggedEntity(document=res.document_id,
-                                                        start=res.start,
-                                                        end=res.end,
-                                                        ent_id=res.ent_id,
-                                                        ent_type=res.ent_type,
-                                                        text=res.ent_str))
-    for doc_id, tags in tag_result.items():
-        doc_results[doc_id].tags = tags
-        doc_results[doc_id].sort_tags()
-
-    # logging.info('Querying for statement extractions...')
     # Next query for extracted statements
-    es_query = session.query(Predication).filter(and_(Predication.document_id.in_(document_ids),
-                                                      Predication.document_collection == document_collection))
+    es_query = session.query(Predication)
+    es_query = es_query.filter(Predication.document_collection == document_collection)
+    es_query = es_query.filter(Predication.document_id.in_(document_ids))
+
     es_for_doc = defaultdict(list)
     sentence_ids = set()
     sentenceid2doc = defaultdict(set)
@@ -252,7 +300,6 @@ def retrieve_narrative_documents_from_database(session, document_ids: Set[int], 
     for doc_id, extractions in es_for_doc.items():
         doc_results[doc_id].extracted_statements = extractions
 
-    # logging.info('Querying for sentences...')
     # Last query for document sentences
     sentence_query = session.query(Sentence).filter(Sentence.id.in_(sentence_ids))
     doc2sentences = defaultdict(list)
@@ -265,8 +312,5 @@ def retrieve_narrative_documents_from_database(session, document_ids: Set[int], 
 
     for doc_id, metadata in doc2metadata.items():
         doc_results[doc_id].metadata = metadata
-
-    for doc_id, classification in doc2classification.items():
-        doc_results[doc_id].classification = {d_class: d_expl for d_class, d_expl in classification}
 
     return list(doc_results.values())
