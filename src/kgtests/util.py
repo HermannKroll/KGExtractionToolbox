4fb24fea4cb9d04a6862a66be2b5f4062399ba8d
HermannKroll <hermann.kroll@gmail.com>
2025-04-03 12:12:17 +0200

    enable logging when testing

--- src/kgtests/util.py
+++ src/kgtests/util.py
@@ -11,6 +11,9 @@ from kgextractiontoolbox.config import ENTITY_LINKING_CONFIG
 
 test_dir = 'src/kgtests/'
 
+logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                    datefmt='%Y-%m-%d:%H:%M:%S',
+                    level=logging.INFO)
 
 def create_test_kwargs():
     config = cnf.Config(ENTITY_LINKING_CONFIG)
